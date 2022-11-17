f973e35252b59175deda5cff8f86ff50b7e25a6d
dreo <79935220+4ndreo@users.noreply.github.com>
2022-11-17 19:18:29 -0300

    localhost

--- services/base.service/database.handler.js
+++ services/base.service/database.handler.js
@@ -1,9 +1,9 @@
 import { MongoClient, ObjectId,ServerApiVersion } from "mongodb";
 import {promise} from "bcrypt/promises.js";
-const uri = "mongodb+srv://admin:cgres123gueiser456andreo789@cgres-back.fe4sygh.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// const uri = "mongodb+srv://admin:cgres123gueiser456andreo789@cgres-back.fe4sygh.mongodb.net/?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-// const client = new MongoClient("mongodb://127.0.0.1:27017");
+const client = new MongoClient("mongodb://127.0.0.1:27017");
 
 async function connectDB(action) {
   // const client = new MongoClient("mongodb://127.0.0.1:27017");
