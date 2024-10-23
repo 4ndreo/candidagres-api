4903fc65ff1a1ed0dafbe53d634fe894a3cb4ab1
dreo <79935220+4ndreo@users.noreply.github.com>
2024-10-22 21:19:17 -0300

    minor fixes

--- middlewares/auth.middlewares.js
+++ middlewares/auth.middlewares.js
@@ -3,7 +3,7 @@ import jwt from "jsonwebtoken";
 const whitelistedEndPoints = [
   "/api/users/login",
   "/api/users/user",
-  "/api/cursos",
+  "/api/classesAll",
   "/api/create_preference",
   "/api/webhook",
 ];
