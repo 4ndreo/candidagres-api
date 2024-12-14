620b4ecd9543a583381459ae1cbc26b366099430
dreo <79935220+4ndreo@users.noreply.github.com>
2024-12-14 02:08:52 -0300

    feat: purchase confirmation email to final user and admin

--- controllers/mp.controller.js
+++ controllers/mp.controller.js
@@ -1,7 +1,11 @@
 import { Preference, MercadoPagoConfig, Payment } from 'mercadopago';
 import * as purchasesService from '../services/purchases.service.js';
 import * as cartsService from '../services/cart.service.js';
+import * as usersService from '../services/users.service.js';
 import { ObjectId } from 'mongodb';
+import { purchaseConfirmationTemplate } from '../config/purchaseConfirmationTemplate.js';
+import transporter from "../config/mailConfig.cjs";
+import { newOrderTemplate } from '../config/newOrderTemplate.js';
 
 
 async function createPreference(req, res) {
@@ -40,7 +44,7 @@ async function receiveWebhook(req, res) {
     if (req.query.hasOwnProperty('data.id')) {
         const client = new MercadoPagoConfig({ accessToken: process.env.MP_ACCESS_TOKEN });
         const payment = new Payment(client);
-        
+
         payment.get({
             id: req.query['data.id'],
         }).then(async (resp) => {
@@ -63,6 +67,51 @@ async function receiveWebhook(req, res) {
                 if (purchase._id) {
                     await cartsService.remove(purchase.id_cart)
                 }
+
+                await usersService.findById(resp.metadata.id_user).then((userData) => {
+                    if (!userData) {
+                        throw new Error('Este correo no pertenece a un usuario.')
+                    } else {
+                        const purchaseData = {
+                            id_user: new ObjectId(resp.metadata.id_user),
+                            id_cart: new ObjectId(resp.metadata.id_cart),
+                            items: resp.additional_info.items,
+                            created_at: new Date(),
+                            totalCost: resp.transaction_amount,
+                            totalQuantity: resp.metadata.total_quantity,
+                            totalDelay: resp.metadata.total_delay,
+                            mp_card: resp.card,
+                            mp_fee_details: resp.fee_details,
+                            mp_id: resp.order.id,
+                            mp_payer: resp.payer,
+                            delivered_at: null
+                        }
+
+                        const mailData = {
+                            from: 'hola@candidagres.com',  // TODO: replace with production mail sender
+                            to: 'franjandreo@gmail.com',   // TODO: replace with user.email
+                            subject: 'Confirmación de compra - Cándida Gres',
+                            html: purchaseConfirmationTemplate(userData, purchaseData),
+                        };
+
+                        const mailDataAdmin = {
+                            from: 'hola@candidagres.com',  // TODO: replace with production mail sender
+                            to: 'franjandreo@gmail.com',   // TODO: replace with admin email
+                            subject: 'Nuevo pedido - Cándida Gres',
+                            html: newOrderTemplate(userData, purchaseData),
+                        };
+
+                        transporter.sendMail(mailData, function (err, info) {
+                            if (err)
+                                throw new Error({ email: userData.email, err: err })
+                        });
+
+                        transporter.sendMail(mailDataAdmin, function (err, info) {
+                            if (err)
+                                throw new Error({ email: userData.email, err: err })
+                        });
+                    }
+                })
             }
         }).catch((err) => {
             console.error(err)
